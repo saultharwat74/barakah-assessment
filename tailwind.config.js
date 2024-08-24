33b34ef2394a00e08765d182b513f616ed59049f
Muhammad Tharwat <mohamedtharout190@gmail.com>
2024-08-24 15:23:59 +0300

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#c3e600",
+        },
+        secondary: {
+          DEFAULT: "#8d9296",
+        },
+      },
+    },
   },
   future: {
     hoverOnlyWhenSupported: true,
